bb6a9ccbc76bfa85e04048f64602b2a1f792348a
Ghetto05 <beckerleon250@gmail.com>
2025-04-08 07:57:09 +0200

    Comments, remove debugging

--- src/extractor.py
+++ src/extractor.py
@@ -5,14 +5,17 @@ log_file_name = "log.txt"
 output_file_name = "output.csv"
 
 def log(message, log_to_console):
+    # optionally: print to console
     if log_to_console:
         print(message)
+    # open log file, write output after new line, close files
     f = open(log_file_name, "a")
     f.write("\n")
     f.write(message)
     f.close()
 
 def init_logger():
+    # create log file
     file = open(log_file_name, "w")
     file.write("")
     file.close()
@@ -20,19 +23,22 @@ def init_logger():
 def main():
     init_logger()
 
+    # prompt user for folder
     folder = input("Please enter path to files: ")
     log(f"User entered path: {folder}", False)
 
+    # check if path exists
     if not os.path.exists(folder):
         log("The specified folder does not exist.", False)
         raise FileNotFoundError("The specified folder does not exist.")
 
+    # gather references to all found files
     files = os.listdir(folder)
-
     csvs = [file for file in files if file.endswith('.csv')]
     xmls = [file for file in files if file.endswith('.xml')]
     jsons = [file for file in files if file.endswith('.json')]
 
+    # do not continue if no files found
     if not (csvs or xmls or jsons):
         log("No CSV, XML, or JSON file in the specified folder.", False)
         raise ValueError("No CSV, XML, or JSON file in the specified folder.")
@@ -42,9 +48,10 @@ def main():
         file_path = os.path.join(folder, file)
         try:
             log(f"Reading file {file}...", True)
+            # read file
             df = pd.read_csv(file_path, sep=';')
+            # append content to combined frame
             data_frames.append(df)
-            df.to_csv("result_csv.csv", sep=';')
         except Exception as e:
             log(f"Could not read file {file}:\n{e}", True)
 
@@ -52,29 +59,31 @@ def main():
         file_path = os.path.join(folder, file)
         try:
             log(f"Reading file {file}...", True)
+            # read file
             df = pd.read_xml(file_path)
+            # append content to combined frame
             data_frames.append(df)
-            df.to_csv("result_xml.csv", sep=';')
         except Exception as e:
             log(f"Could not read file {file}: {e}", True)
 
+    # load all json files
     for file in jsons:
         file_path = os.path.join(folder, file)
         try:
             log(f"Reading file {file}...", True)
+            # read file
             df = pd.read_json(file_path)
+            # append content to combined frame
             data_frames.append(df)
-            df.to_csv("result_json.csv", sep=';')
         except Exception as e:
             log(f"Could not read file {file}: {e}", True)
 
+    # check if data combination was successful
     if data_frames:
         combined_df = pd.concat(data_frames, ignore_index=True)
         log("Data combined successfully.", True)
     else:
         raise ValueError("No data could be combined.")
-
-    print(combined_df)
 
     log(f"Saving results to {output_file_name}", True)
     combined_df.to_csv(output_file_name, index=False, sep=';')
